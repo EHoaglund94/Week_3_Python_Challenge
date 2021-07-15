2c44ffc4f79f84a60bcadce6b8a19d5679d0a268
Erik Hoaglund <ehoaglund94@gmail.com>
2021-07-14 19:00:36 -0500

    Added variables and code to read csv files

---
+++ PyBank/main.py
@@ -0,0 +1,24 @@
+import os
+import csv
+
+#Path to collect data form the Resources folder
+bank_csv = os.path.join('Resources', 'budget_data.csv')
+
+#Variables
+month_list = []
+revenue_list = []
+total_revenue = 0
+total_change = 0
+change_max = [ '', 0]
+change_min = [ '', 0]
+
+
+
+
+with open (bank_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    header = next(csvfile)
+
+
+
+   

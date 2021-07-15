1f232a885dd975d69ac0df38373042df1b8d08dc
Erik Hoaglund <ehoaglund94@gmail.com>
2021-07-15 00:39:53 -0500

    Added code to export results to text files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,6 @@
 import os
 import csv
-from typing import TYPE_CHECKING
+
 
 #Path to collect data form the Resources folder
 bank_csv = os.path.join('Resources', 'budget_data.csv')
@@ -14,7 +14,7 @@ change_max = [ '', 0]
 change_min = [ '', 0]
 
 
-
+#read CSV file
 with open (bank_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
     header = next(csvfile)
@@ -32,6 +32,7 @@ with open (bank_csv, 'r') as csvfile:
 
 #loop through lists to find Financial Analysis outputs
 total_months = len(month_list)
+
 
 for i in range(1,total_months): 
     month_change = revenue_list[i] - revenue_list[i-1]
@@ -55,9 +56,16 @@ line6 = (f'Greatest Increase in Profits: {change_max[0]}  ({change_max[1]})')
 line7 = (f'Greatest Decrease in Profits:  {change_min[0]}  ({change_min[1]})')
 summary_data.extend([line1,line2,line3,line4,line5,line6,line7])
 
+#Output in terminal
 for line in summary_data:
     print(line)
 
 
+
+#output to text file
+output_text = open('PyBank_outputs.txt', 'w')
+with open('PyBank_outputs.txt', 'w') as txt_file:
+    for line in summary_data:
+        txt_file.write(line + '\n')
 
 

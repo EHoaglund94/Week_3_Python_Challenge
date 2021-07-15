49de7896f6520fe8b0e764a4cca366f43a96729f
Erik Hoaglund <ehoaglund94@gmail.com>
2021-07-15 12:51:26 -0500

    Added code to calculate summary table values

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,10 +4,10 @@ import csv
 poll_csv = os.path.join('Resources','election_data.csv')
 
 #Set variables
-output_keys = ['Candidate', 'Number of Votes', 'Percentage of Votes']
 voter_id = []
 candidates = []
 candidate_votes = {}
+winner_vote = 0
 
 
 
@@ -18,13 +18,30 @@ with open (poll_csv, 'r') as csvfile:
 
     #Loop through CSV file to count number of voters, candidates, and votes for candidates
     for row in csvreader:
-        candidate = row['Candidate']
-        voter_id.append(row['Voter ID'])
+        candidate = row[2]
+        voter_id.append(row[0])
         if candidate not in candidates:
             candidates.append(candidate)
             candidate_votes[candidate] = 1
         else:
             candidate_votes[candidate] += 1
+
+
+#Summary data calculations
+total_votes = len(voter_id)
+
+candidate_percentage = {candidate : candidate_votes[candidate]/total_votes\
+                        for candidate in candidates}
+for item in candidate_percentage.items():
+    if item[1] > winner_vote: 
+        winner_vote = item[1]
+        winner = item[0]
+
+
+
+
+
+
 
     
 

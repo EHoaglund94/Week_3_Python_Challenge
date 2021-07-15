90edf1b8301fea94915a704804c0f5dfa511896c
Erik Hoaglund <ehoaglund94@gmail.com>
2021-07-14 22:42:59 -0500

    Added For loop to run through lists Created code to find total number of months and find the average of the month to month changes Created test print commands to check outputs

--- PyBank/main.py
+++ PyBank/main.py
@@ -17,6 +17,10 @@ change_min = [ '', 0]
 with open (bank_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
     header = next(csvfile)
+    
+
+
+
     #Read through rows in CSV file
     for row in csvreader:
         month_year = row[0]
@@ -25,5 +29,14 @@ with open (bank_csv, 'r') as csvfile:
         revenue_list.append(revenue)
         total_revenue += revenue
 
+#loop through lists to find Financial Analysis outputs
+total_months = len(month_list)
 
-   
+for i in range(1,total_months): 
+    month_change = revenue_list[i] - revenue_list[i-1]
+    total_change += month_change
+
+
+average_change = total_change / (total_months-1 )
+print (average_change)
+print(total_months)

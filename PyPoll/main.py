081fb4bb3f2b3d0ca2059891c9be36fdd216c50f
Erik Hoaglund <ehoaglund94@gmail.com>
2021-07-15 12:02:54 -0500

    Set variables to grab data from csv

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,6 +3,12 @@ import csv
 
 poll_csv = os.path.join('Resources','election_data.csv')
 
+#Set variables
+output_keys = ['Candidate', 'Number of Votes', 'Percentage of Votes']
+voter_id = []
+candidates = []
+candidate_votes = {}
+
 
 
 with open (poll_csv, 'r') as csvfile:
